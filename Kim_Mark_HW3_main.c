23913fd25c26a88d5eb660d4db57527734df3adc
student <mkim797@yahoo.com>
2022-09-22 19:39:17 -0700

    finish code commenting

--- Kim_Mark_HW3_main.c
+++ Kim_Mark_HW3_main.c
@@ -8,6 +8,8 @@
 * File: Kim_Mark_HW3_main.c
 *
 * Description:
+* This is a simple shell that takes in user input and runs the
+* commands entered along with any command arguments for Linux.
 *
 **************************************************************/
 #include <stdlib.h>
@@ -20,14 +22,19 @@
 #define BUFFER_SIZE  123
 
 int main(int argc, char *argv[]) {
+    // Buffer, shell arguments, and tokens
     char buf[BUFFER_SIZE];
     int sh_argc;
     char *sh_argv[BUFFER_SIZE/2];
     char *token;
     char *prompt;
+
+    // Child status and process id
     int c_stat;
     pid_t c_pid;
 
+    // This changes the prompt to whatever is provided as a command
+    // line argument or ">" as default. (Requirement #8 in Requirements.md)
     if (argc > 1) {
         prompt = argv[1];
     } else {
@@ -35,63 +42,80 @@ int main(int argc, char *argv[]) {
     }
     
     while(1) {
-        printf("%s", prompt);
+        printf("%s", prompt);  // The prompt for the shell
 
+        // Takes in user input and writes it to buffer.
+        // As specified in linux man page documentation, NULL is returned
+        // if EOF is reached from fgets, hence shell will exit gracefully per
+        // requirements (Error Handling #2 in Requirements.md)
         if(fgets(buf, BUFFER_SIZE, stdin) == NULL) {
             printf("\nThank you for using my command shell.\n");
             return 0;
         }
 
-        // printf("strlen(buf): %lu\n",strlen(buf));
-
+        // In the chance that user input exceeds the buffer size
+        // the buffer is cleared by iterating through the rest of
+        // standard input with fgetc() until a new line is reached.
+        // 
         if (strlen(buf) >= BUFFER_SIZE - 1) {
             while(fgetc(stdin) != '\n');
         }
-
-        // printf("Buffer: '%s'\tBuffer size: %lu\n", buf, sizeof(buf));
     
-        token = strtok(buf, " \n");
-
+        // We tokenize the buffer and enter all the tokens into
+        // the shell argument vector all the while incrementing
+        // the argument count. A NULL is placed at the end of the array
+        // per man page specifications for execvp.
+        // (Requirements #4 & #6 in Requirements.md)
+        token = strtok(buf, " \n\t");
         sh_argc = 0;
-
         while (token != NULL) {
             sh_argv[sh_argc++] = token;
             printf("Token: '%s'\n", token);
-            token = strtok(NULL, " \n");
+            token = strtok(NULL, " \n\t");
         }
-
         sh_argv[sh_argc++] = NULL;
 
-        // printf("sh_argc: %d\n", sh_argc);
-
+        // sh_argv[0] will be NULL when a user inputs empty lines
+        // or lines of whitespace are entered. As specified,
+        // an error is reported and a new line of input is fetched,
+        // (Error Handling #3 in Requirements.md)
         if (sh_argv[0] == NULL) {
-            perror("An empty line was entered. Please try again.");
+            perror("Error: An empty line was entered. Please try again.");
             continue;
-        } else if(!strcmp(sh_argv[0], "exit")) {
+        } 
+        // The exit command shall terminate the shell, so a strcmp
+        // is used to check if the first argument in the argument vector
+        // is exit. (Requirement #7 in Requirements.md)
+        else if(!strcmp(sh_argv[0], "exit")) {
             printf("\nThank you for using my command shell.\n");
             return 0;
         } else {
-            for (int i = 0; i < sh_argc; i++) {
-                printf("sh_argv[%d]: '%s'\n", i, sh_argv[i]);
-            }
-
+            // Per man page specification, fork() returns -1 if it failed,
+            // 0 if this is the child process, or the child process id 
+            // if this is the parent process. execvp() is called in the child
+            // process per specifications. (Requirement #1 & #3 in
+            // Requirements.md)
             c_pid = fork();
-
             if ( c_pid < 0 ) {
                 perror("Fork failed: exiting");
                 return (-1);
             } else if ( c_pid == 0 ) {
+                // Child process.
+                // Per man page specification, execvp() returns -1 if it fails
+                // this handles that case while executing execvp().
                 if(execvp(sh_argv[0], sh_argv) < 0) {
                     perror("Execvp failed: exiting");
                     return (-1);
                 }
             } else {
+                // Parent process:
+                // Per specification, the shell waits for child process to
+                // complete, then prints the child PID and the result from
+                // the child process. (Requirement #2 in Requirements.md)
                 wait(&c_stat);
                 printf("Child %d, exited with %d\n", c_pid, WEXITSTATUS(c_stat));
             }
         }
-        
     }
-
     return 0;
 }
